fe04ba097925355d68db3dced22ad5363d1eaefa
Camilo Cárdenas <camicardenasp@gmail.com>
2022-12-12 12:07:47 -0500

    Agregamos botón de devolverse y prevenimos rompimiento en la página final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import './App.css'
-import CharacterList from '../components/CharacterList';
+import CharacterList from './components/CharacterList'
 
 
 function App() {
   return (
-    <div className="App container  bg-dark text-white py-3">
+    <div className="bg-dark text-white py-3">
       <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
       <CharacterList/>
     </div>
